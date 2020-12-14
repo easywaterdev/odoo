56b7b6b8bffe4c63b0bc96a094662e0a50bccb35
captivea-cko <55201842+captivea-cko@users.noreply.github.com>
2020-12-14 11:30:14 -0500

    other address renaming module

---
+++ cap_remove_private_address/models/respartner.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+	_inherit = 'res.partner'
+    
+	type = fields.Selection(
+	[('contact', 'Contact'),
+	 ('invoice', 'Billing address'),
+	 ('delivery', 'Job address'),
+	 ('other', 'Installation address'),
+	 ('private', 'Private address'),
+	], string='Address Type',
+	default='contact',
+        help="Used by Sales and Purchase Apps to select the relevant address depending on the context.")
+	
+
