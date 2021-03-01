53d6368f3ba905cae68100424d108774021f14c8
rto-odoo <rtorres@opensourceintegrators.com>
2021-03-01 15:29:14 -0800

    added product_configurator_sale module

---
+++ product_configurator_sale/models/sale.py
@@ -0,0 +1,59 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_config_start(self):
+        """Return action to start configuration wizard"""
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.configurator.sale',
+            'name': "Product Configurator",
+            'view_mode': 'form',
+            'target': 'new',
+            'context': dict(
+                self.env.context,
+                default_order_id=self.id,
+                wizard_model='product.configurator.sale',
+            ),
+        }
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    custom_value_ids = fields.One2many(
+        comodel_name='product.attribute.value.custom',
+        inverse_name='product_id',
+        related="product_id.value_custom_ids",
+        string="Custom Values"
+    )
+    config_ok = fields.Boolean(
+        related="product_id.config_ok",
+        string="Configurable",
+        readonly=True
+    )
+
+    @api.multi
+    def reconfigure_product(self):
+        """ Creates and launches a product configurator wizard with a linked
+        template and variant in order to re-configure a existing product. It is
+        esetially a shortcut to pre-fill configuration data of a variant"""
+        wizard_model = 'product.configurator.sale'
+
+        extra_vals = {
+            'order_id': self.order_id.id,
+            'order_line_id': self.id,
+            'product_id': self.product_id.id,
+        }
+
+        self = self.with_context({
+            'default_order_id': self.order_id.id,
+            'default_order_line_id': self.id
+        })
+
+        return self.product_id.product_tmpl_id.create_config_wizard(
+            model_name=wizard_model, extra_vals=extra_vals
+        )
---
+++ product_configurator_sale/wizard/product_configurator.py
@@ -0,0 +1,56 @@
+from odoo import api, fields, models
+
+
+class ProductConfiguratorSale(models.TransientModel):
+
+    _name = 'product.configurator.sale'
+    _inherit = 'product.configurator'
+
+    order_id = fields.Many2one(
+        comodel_name='sale.order',
+        required=True,
+        readonly=True
+    )
+    order_line_id = fields.Many2one(
+        comodel_name='sale.order.line',
+        readonly=True
+    )
+
+    def _get_order_line_vals(self, product_id):
+        """ Hook to allow custom line values to be put on the newly
+        created or edited lines."""
+
+        line_vals = {
+            'product_id': product_id,
+            'order_id': self.order_id.id
+        }
+
+        extra_vals = self.order_line_id._prepare_add_missing_fields(line_vals)
+        line_vals.update(extra_vals)
+        return line_vals
+
+    @api.multi
+    def action_config_done(self):
+        """Parse values and execute final code before closing the wizard"""
+        res = super(ProductConfiguratorSale, self).action_config_done()
+        if res.get('res_model') == self._name:
+            return res
+        line_vals = self._get_order_line_vals(res['res_id'])
+
+        # To call onchange explicite as write and create
+        # will not trigger onchange automatically
+        order_line_obj = self.env['sale.order.line']
+        specs = order_line_obj._onchange_spec()
+        updates = order_line_obj.onchange(line_vals, ['product_id'], specs)
+        values = updates.get('value', {})
+        for name, val in values.items():
+            if isinstance(val, tuple):
+                values[name] = val[0]
+        line_vals.update(values)
+
+        if self.order_line_id:
+            self.order_line_id.write(line_vals)
+        else:
+            self.order_id.write({'order_line': [(0, 0, line_vals)]})
+
+        return
