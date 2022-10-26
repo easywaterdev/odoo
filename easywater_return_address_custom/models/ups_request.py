a71cc72bc12512c0f67ef828855c762afd5ec5ac
OYBI-Jordan-Fleetwood <jordan.fleetwood@oybi.com>
2022-10-26 16:58:42 -0400

    Update

--- easywater_return_address_custom/models/ups_request.py
+++ easywater_return_address_custom/models/ups_request.py
@@ -438,16 +438,18 @@ class UPSRequest():
         shipment.ShipFrom.Phone = self.factory_ns2.ShipPhoneType()
         shipment.ShipFrom.Phone.Number = self._clean_phone_number(ship_from.phone)
 
+        user = self.env['res.users'].search(['user_ids.id', '=', ship_to.id])
+
         shipment.ShipTo = self.factory_ns2.ShipToType()
         shipment.ShipTo.Address = self.factory_ns2.ShipToAddressType()
         shipment.ShipTo.AttentionName = (ship_to.name or '')[:35]
         shipment.ShipTo.Name = (ship_to.parent_id.name or ship_to.name or '')[:35]
-        shipment.ShipTo.Address.AddressLine = [l for l in [ship_to.private_street or '', ship_to.private_street2 or ''] if l]
-        shipment.ShipTo.Address.City = ship_to.private_city or ''
-        shipment.ShipTo.Address.PostalCode = ship_to.private_zip or ''
-        shipment.ShipTo.Address.CountryCode = ship_to.private_country_id.code or ''
-        if ship_to.private_country_id.code in ('US', 'CA', 'IE'):
-            shipment.ShipTo.Address.StateProvinceCode = ship_to.private_state_id.code or ''
+        shipment.ShipTo.Address.AddressLine = [l for l in [user.private_street or '', user.private_street2 or ''] if l]
+        shipment.ShipTo.Address.City = user.private_city or ''
+        shipment.ShipTo.Address.PostalCode = user.private_zip or ''
+        shipment.ShipTo.Address.CountryCode = user.private_country_id.code or ''
+        if user.private_country_id.code in ('US', 'CA', 'IE'):
+            shipment.ShipTo.Address.StateProvinceCode = user.private_state_id.code or ''
         shipment.ShipTo.Phone = self.factory_ns2.ShipPhoneType()
         shipment.ShipTo.Phone.Number = self._clean_phone_number(shipment_info['phone'])
         if not ship_to.commercial_partner_id.is_company:
