5f57689e4149f971473ff848c43846ec471fa942
OYBI-Jordan-Fleetwood <jordan.fleetwood@oybi.com>
2022-10-26 22:09:13 -0400

    Update

--- easywater_return_address_custom/__manifest__.py
+++ easywater_return_address_custom/__manifest__.py
@@ -10,7 +10,7 @@
     'author': "OYBI",
     'website': "https://www.oybi.com",
     'category': 'Extra Tools',
-    'version': '15.0.0.31',
+    'version': '15.0.0.32',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
