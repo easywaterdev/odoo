924cb86ad7a579edb134a4617257a11900ea02ae
OYBI-Jordan-Fleetwood <jordan.fleetwood@oybi.com>
2022-10-26 21:29:46 -0400

    Update

--- easywater_return_address_custom/models/delivery_ups.py
+++ easywater_return_address_custom/models/delivery_ups.py
@@ -64,10 +64,10 @@ class ProviderUPS(models.Model):
 
         package_type = picking.package_ids and picking.package_ids[0].package_type_id.shipper_package_code or self.ups_default_package_type_id.shipper_package_code
 
-        if self.partner_id.private_street and  self.partner_id.private_city and  self.partner_id.private_state_id and  self.partner_id.private_zip and  self.partner_id.private_country_id:
+        if picking.partner_id.private_street and picking.partner_id.private_city and   picking.partner_id.private_state_id and   picking.partner_id.private_zip and  picking.partner_id.private_country_id:
             srm.send_shipping(
                 shipment_info=shipment_info, packages=packages, shipper=picking.partner_id, ship_from=picking.partner_id,
-                ship_to=self.partner_id, packaging_type=package_type, service_type=ups_service_type, duty_payment='RECIPIENT', label_file_type=self.ups_label_file_type, ups_carrier_account=ups_carrier_account,
+                ship_to=picking.partner_id, packaging_type=package_type, service_type=ups_service_type, duty_payment='RECIPIENT', label_file_type=self.ups_label_file_type, ups_carrier_account=ups_carrier_account,
                 saturday_delivery=picking.carrier_id.ups_saturday_delivery, cod_info=cod_info)
             srm.return_label()
         else:
