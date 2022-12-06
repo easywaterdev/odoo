9c8169f424e682b5b581cb8a02b63706af3de5a7
OYBI-Jordan-Fleetwood <jordan.fleetwood@oybi.com>
2022-12-06 11:38:52 -0500

    Update

--- easywater_sale_order_custom/models/sale_order.py
+++ easywater_sale_order_custom/models/sale_order.py
@@ -5,18 +5,17 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-
-    def _default_carrier_id(self):
-        user = self.env['res.users'].search([('id', '=', self._uid)])
-        if user.team_id.name == 'Commercial Sales':
-            return self.env['delivery.carrier'].search([('name', '=', 'Custom Freight Quote')]).id
-        else:
-            return ''
-
-    carrier_id = fields.Many2one(default=_default_carrier_id, required=True)
+    carrier_id = fields.Many2one(required=True)
 
     def action_quotation_send(self):
         if not self.carrier_id:
             raise UserError("You cannot mark quotations as sent until you enter a carrier!")
         else:
             return super(SaleOrder, self).action_quotation_send()
+
+    @api.onchange('team_id')
+    def _set_default_carrier(self):
+        for record in self:
+            if record.team_id:
+                if record.team_id.name == 'Commercial Sales':
+                    record.carrier_id = record.env['delivery.carrier'].search([('name', '=', 'Custom Freight Quote')]).id
