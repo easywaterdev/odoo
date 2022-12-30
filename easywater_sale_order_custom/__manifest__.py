fd08a032e4f38e9ce614afc3c3e86315cc4d5d1a
oybiodoo <brian@oybi.com>
2022-12-30 10:33:07 -0500

    making carrier_id read only

--- easywater_sale_order_custom/__manifest__.py
+++ easywater_sale_order_custom/__manifest__.py
@@ -7,14 +7,14 @@
         This customization was written by OYBI for Easywater. The purpose of this customization is to add validation to the Delivery Method on sales orders. 
         1. You cannot mark a quotation as sent until you have selected a Delivery Method. (Unless the sales team is "Commercial Sales")
         2. You cannot confirm a quotation until you have selected a Delivery Method. (Unless the sales team is "Commercial Sales")
-        3. Adds the delivery method (Carrier ID) to the sales order form view underneath payment method.
+        3. Adds the delivery method (Carrier ID) to the sales order form view underneath payment method (read only).
         4. Defaults the delivery method to blank. If the sales team is "Commercial Sales" then the delivery method is defaulted to "Custom Freight Quote".
  """,
 
     'author': "OYBI",
     'website': "https://www.oybi.com",
     'category': 'Extra Tools',
-    'version': '15.0.0.23',
+    'version': '15.0.0.24',
     'license': 'OPL-1',
     'application': False,
     'installable': True,
