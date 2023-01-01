baa29c93286caf005cfb782668cd0de00dd0648f
oybiodoo <61978207+oybiodoo@users.noreply.github.com>
2023-01-01 14:10:53 -0500

    installing SO custom (delivery method validation) and avatax fix (calcualte SO on Mark Sent)

---
+++ easywater_sale_order_custom/models/sale_order.py
@@ -0,0 +1,45 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+#   this event is when sending via email
+    def action_quotation_send(self):
+        res = super(SaleOrder, self).action_quotation_send()
+        for record in self:
+            if not record.team_id or not record.team_id.name == "Commercial Sales":
+                if not record.carrier_id:
+                    raise ValidationError("You cannot mark Quotations as sent until you enter a delivery method!")
+        return res
+
+#   this event is when using the action "Mark Quotation as Sent"
+    def action_quotation_sent(self):
+        res = super(SaleOrder, self).action_quotation_sent()
+        for record in self:
+            if not record.team_id or not record.team_id.name == "Commercial Sales":
+                if not record.carrier_id:
+                    raise ValidationError("You cannot mark Quotations as sent until you enter a delivery method!")
+        return res
+
+    @api.onchange('team_id')
+    def _set_default_carrier(self):
+        for record in self:
+            if record.team_id:
+                if record.team_id.name == 'Commercial Sales':
+                    record.carrier_id = record.env['delivery.carrier'].search([('name', '=', 'Custom Freight Quote')]).id
+                else:
+                    record.carrier_id = ''
+            else:
+                record.carrier_id = ''
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for record in self:
+            if record.state == "sale":
+                if record.team_id:
+                    if record.team_id.name != 'Commercial Sales':
+                        if not record.carrier_id:
+                            raise ValidationError("You cannot confirm a Sales Order until you enter a delivery method!")
+        return res
